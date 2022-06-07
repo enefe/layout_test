e28ba8d828402294dc0f3ba5ced6d1f08ad4df0d
enefe815 <enefe815@yandex.ru>
2022-06-07 17:19:28 +0300

    feat: slider comments

--- js/script.js
+++ js/script.js
@@ -28,3 +28,105 @@ let logoFooter = document.querySelector('.footer__logo');
 if (window.innerWidth >= 1200) {
     logoFooter.setAttribute('src', 'images/logotype blue desktop.png');
 }
+
+// Слайдер комментариев
+const slider = document.querySelector('.comments__container'); // контейнер слайдов
+const slides = Array.from(document.querySelectorAll('.comment')); // массив всех слайдов
+const navs = Array.from(document.querySelectorAll('.nav'));
+const navigation = document.querySelector('.navigation');
+
+let isDragging = false; // нажатие на слайд
+let startPos = 0; // начальная позиция перетаскивания
+let currentTranslate = 0; // наше будущее значение transform для контейнера
+let prevTranslate = 0;
+let animationID = 0;
+let currentIndex = 0; // текущий номер слайда
+let currentNav = 0;
+
+slides.forEach((slide, index) => {
+    // касания экрана:
+    slide.addEventListener('touchstart', touchStart(index));
+    slide.addEventListener('touchend', touchEnd);
+    slide.addEventListener('touchmove', touchMove);
+
+    //касания мыши:
+    slide.addEventListener('mousedown', touchStart(index));
+    slide.addEventListener('mouseup', touchEnd);
+    slide.addEventListener('mouseleave', touchEnd);
+    slide.addEventListener('mousemove', touchMove);
+})
+
+function touchStart(index) {
+    return function(event) {
+        currentIndex = index;
+        currentNav = index;
+        startPos = getPositionX(event);
+        isDragging = true;
+
+        animationID = requestAnimationFrame(animation);
+        slider.classList.add('grabbing');
+    }
+}
+
+function touchEnd() {
+    isDragging = false;
+    cancelAnimationFrame(animationID);
+
+    const movedBy = currentTranslate - prevTranslate;
+
+    if (movedBy < -100 && currentIndex < slides.length - 1) {
+        currentIndex += 1;
+
+        for (let i = 0; i < navs.length; i++) {
+            if (currentIndex == i) {
+                navs[i].classList.add('nav__active');
+                navs[i - 1].classList.remove('nav__active');
+            }
+        }
+
+    }
+
+    if (movedBy > 100 && currentIndex > 0) {
+        currentIndex -= 1;
+
+        for (let i = 0; i < navs.length; i++) {
+            if (currentIndex == i) {
+                navs[i].classList.add('nav__active');
+                navs[i + 1].classList.remove('nav__active');
+            }
+        }
+    }
+
+    setPositionByIndex();
+
+    slider.classList.remove('grabbing');
+}
+
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event);
+        currentTranslate = prevTranslate + currentPosition - startPos
+    }
+}
+
+function getPositionX(event) {
+    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+}
+
+function animation() {
+    setSliderPosition();
+
+    if (isDragging) {
+        requestAnimationFrame(animation);
+    }
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -window.innerWidth;
+    prevTranslate = currentTranslate;
+    setSliderPosition();
+}
